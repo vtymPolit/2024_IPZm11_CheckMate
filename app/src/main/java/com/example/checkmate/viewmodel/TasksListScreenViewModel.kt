abd0dc13067457beb4e6af08af8805f4f6e7113d
Andrii Antoniuk IPZm-11 <antonyuk.a0912.121.24@lntu.edu.ua>
2024-11-13 19:15:43 +0200

    db:fetch tasks list method

---
+++ app/src/main/java/com/example/checkmate/viewmodel/TasksListScreenViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.checkmate.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.checkmate.data.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class TasksListScreenViewModel : ViewModel() {
+    private val _tasksList = MutableStateFlow<List<Task>>(emptyList())
+    val tasksList: StateFlow<List<Task>> = _tasksList.asStateFlow()
+
+    private val db = Firebase.firestore
+    private val user = Firebase.auth.currentUser
+
+    fun getTasksListFromFirebase() {
+        db.collection(user?.uid.toString()).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val tasks = task.result.toObjects(Task::class.java)
+                _tasksList.value = tasks
+            }
+        }
+    }
+}
