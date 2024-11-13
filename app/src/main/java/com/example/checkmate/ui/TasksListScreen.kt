3e3d4f7f7d928ae3d6f1578be65d547943a7353d
Andrii Antoniuk IPZm-11 <antonyuk.a0912.121.24@lntu.edu.ua>
2024-11-13 21:33:59 +0200

    task-list:tasks list screen

--- app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
+++ app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
@@ -1,46 +1,54 @@
 package com.example.checkmate.ui
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.checkmate.data.Task
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-import com.google.firebase.firestore.firestore
+import com.example.checkmate.viewmodel.TasksListScreenViewModel
 
 @Composable
-fun TasksListScreen(){
-    val db = Firebase.firestore
-    val user = Firebase.auth.currentUser
-    user?.let {
-        val name = it.displayName
-        val uid = it.uid
-    }
-    val list = remember {
-        mutableStateOf(emptyList<Task>())
-    }
-    db.collection(user?.uid.toString()).get().addOnCompleteListener { task ->
-        if( task.isSuccessful){
-            list.value = task.result.toObjects(Task::class.java)
+fun TasksListScreen(navController: NavController, tasksViewModel: TasksListScreenViewModel) {
+    val tasks by tasksViewModel.tasksList.collectAsState()
+    LaunchedEffect(Unit) { tasksViewModel.getTasksListFromFirebase() }
+    LazyColumn {
+        items(tasks) {
+            TaskItem(
+                task = it,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+            )
         }
     }
-        Box(
-        modifier = Modifier.fillMaxSize(),
-    ){
-        LazyColumn(
+}
 
-        ) {
-            items(list.value) { item: Task ->
-                Card() {
-                    Text(text = item.name)
-                }
-            }
+@Composable
+fun TaskItem(
+    task: Task,
+    modifier: Modifier = Modifier,
+) {
+    Card(modifier = modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = task.name,
+                style = TextStyle(
+                    fontSize = 24.sp,
+                )
+            )
         }
     }
 }
