d249f421a72872a74182e7a0df64525156358fb5
Vlad Tymchuk <tymchuk.v0906.121.24@lntu.edu.ua>
2024-11-17 18:49:15 +0200

    nav:create task composable element

--- app/src/main/java/com/example/checkmate/MainActivity.kt
+++ app/src/main/java/com/example/checkmate/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.checkmate.ui.CreateTaskScreen
 import com.example.checkmate.ui.GoogleSignInScreen
 import com.example.checkmate.ui.TasksListScreen
 import com.example.checkmate.ui.theme.CheckMateTheme
@@ -42,6 +43,9 @@ class MainActivity : ComponentActivity() {
                         }
                         composable("TasksListScreen") {
                             TasksListScreen(navController, tasksViewModel)
+                        }
+                        composable("CreateTaskScreen") {
+                            CreateTaskScreen(navController, tasksViewModel)
                         }
                     }
                 )
