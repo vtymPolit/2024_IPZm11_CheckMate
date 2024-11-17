4beac8f1bc1b9f3a4ed8e5c6de0f48af003de547
Vlad Tymchuk <tymchuk.v0906.121.24@lntu.edu.ua>
2024-11-17 21:28:10 +0200

    create-task:create task screen

--- app/src/main/java/com/example/checkmate/ui/CreateTaskScreen.kt
+++ app/src/main/java/com/example/checkmate/ui/CreateTaskScreen.kt
@@ -1,24 +1,36 @@
 package com.example.checkmate.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.checkmate.data.Task
 import com.example.checkmate.viewmodel.TasksListScreenViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTaskScreen(navController: NavController, tasksViewModel: TasksListScreenViewModel) {
+    var name by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -36,6 +48,34 @@ fun CreateTaskScreen(navController: NavController, tasksViewModel: TasksListScre
         Box(modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row {
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = { name = it },
+                        label = { Text(text = "Name") }
+                    )
+                }
+                Row {
+                    OutlinedTextField(
+                        value = description,
+                        onValueChange = { description = it },
+                        label = { Text(text = "Description") }
+                    )
+                }
+                Button(onClick = {
+                    if (name.isNotBlank()) {
+                        val newTask = Task(name = name, description = description)
+                        tasksViewModel.createTask(newTask)
+                        navController.popBackStack()
+                    }
+                }) {
+                    Text(text = "Create")
+                }
+            }
         }
     }
 }
