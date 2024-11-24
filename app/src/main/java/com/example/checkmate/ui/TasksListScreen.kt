078ca9051575403d2be92f526b67aa3b58e168c4
Stas Shmarovoz <smarovoz84@gmail.com>
2024-11-24 23:22:11 +0200

    done-undone:completion checkbox on task in tasks list

--- app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
+++ app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
@@ -17,9 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -62,6 +67,7 @@ fun TasksListScreen(navController: NavController, tasksViewModel: TasksListScree
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(8.dp),
+                    tasksViewModel = tasksViewModel
                 )
             }
         }
@@ -72,13 +78,23 @@ fun TasksListScreen(navController: NavController, tasksViewModel: TasksListScree
 fun TaskItem(
     task: Task,
     modifier: Modifier = Modifier,
+    tasksViewModel: TasksListScreenViewModel
 ) {
+    var completed by remember { mutableStateOf(false) }
+    completed = task.completed
     Card(modifier = modifier) {
         Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = completed,
+                onCheckedChange = {
+                    tasksViewModel.completedChange(task.id, completed)
+                    completed = it
+                })
+            val completedStyle = if (completed) TextDecoration.LineThrough else null
             Text(
                 text = task.name,
                 style = TextStyle(
                     fontSize = 24.sp,
+                    textDecoration = completedStyle
                 )
             )
         }
