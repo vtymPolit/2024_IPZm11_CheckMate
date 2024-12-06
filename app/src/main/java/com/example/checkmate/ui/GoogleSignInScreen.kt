842a6ef0fa5dde41624ecccd507e5b5c950fa686
Serhii Gyzovatiy <feodalop@i.ua>
2024-12-07 00:22:46 +0200

    feat: logo in SignInScreen

--- app/src/main/java/com/example/checkmate/ui/GoogleSignInScreen.kt
+++ app/src/main/java/com/example/checkmate/ui/GoogleSignInScreen.kt
@@ -1,25 +1,39 @@
 package com.example.checkmate.ui
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.checkmate.R
 import com.example.checkmate.ui.theme.CheckMateTheme
 
 @Composable
 fun GoogleSignInScreen(onGoogleSignInClick: () -> Unit) {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = ""
+        )
+        Spacer(Modifier.height(16.dp))
         Button(
             onClick = {
                 onGoogleSignInClick()
-            },) {
+            },
+        ) {
             Text(text = "Google Sign-In")
         }
     }
