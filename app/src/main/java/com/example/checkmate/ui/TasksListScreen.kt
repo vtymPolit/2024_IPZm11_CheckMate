1f188b9a236b2094e53fe9b138e098b791d88146
Serhii Gyzovatiy <feodalop@i.ua>
2024-12-08 10:41:31 +0200

    feat: task deletion and signing out confirmation pop-up

--- app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
+++ app/src/main/java/com/example/checkmate/ui/TasksListScreen.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
@@ -34,11 +36,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.example.checkmate.data.Task
 import com.example.checkmate.viewmodel.TasksListScreenViewModel
@@ -50,7 +52,7 @@ import com.google.firebase.auth.auth
 fun TasksListScreen(navController: NavController, tasksViewModel: TasksListScreenViewModel) {
     val tasks by tasksViewModel.tasksList.collectAsState()
     val searchQuery by tasksViewModel.searchQuery.collectAsState()
-
+    var openDialogSignOutConfirm by remember { mutableStateOf(false) }
     LaunchedEffect(Unit) { tasksViewModel.getTasksListFromFirebase() }
     Scaffold(
         topBar = {
@@ -61,11 +63,19 @@ fun TasksListScreen(navController: NavController, tasksViewModel: TasksListScree
                 actions = {
                     Button(
                         onClick = {
-                            Firebase.auth.signOut()
-                            navController.navigate("GoogleSignInScreen")
+                            openDialogSignOutConfirm = !openDialogSignOutConfirm
                         }
                     ) {
                         Text(text = "Sign out")
+                        if (openDialogSignOutConfirm) {
+                            DialogConfirm(
+                                onDismissRequest = { openDialogSignOutConfirm = !openDialogSignOutConfirm },
+                                onConfirmation = { Firebase.auth.signOut()
+                                    navController.navigate("GoogleSignInScreen")
+                                    openDialogSignOutConfirm = !openDialogSignOutConfirm},
+                                dialogTitle = "Sign Out?"
+                            )
+                        }
                     }
                 }
             )
@@ -134,7 +144,7 @@ fun TaskItem(
             )
         }
         if (expanded) {
-            TaskItemExpanded(task.description,tasksViewModel, task, navController)
+            TaskItemExpanded(task.description, tasksViewModel, task, navController)
         }
     }
 }
@@ -161,8 +171,9 @@ fun TaskItemExpanded(
     taskDescription: String,
     tasksViewModel: TasksListScreenViewModel,
     task: Task,
-    navController: NavController
+    navController: NavController,
 ) {
+    var openDialogDestroyConfirm by remember { mutableStateOf(false) }
     Row(Modifier.padding(16.dp)) {
         Text(text = taskDescription)
     }
@@ -171,13 +182,65 @@ fun TaskItemExpanded(
             .fillMaxWidth()
             .padding(8.dp), horizontalArrangement = Arrangement.Center
     ) {
-        Button(onClick = {tasksViewModel.setSelectedTask(task)
-            navController.navigate("UpdateTaskScreen")}) {
+        Button(onClick = {
+            tasksViewModel.setSelectedTask(task)
+            navController.navigate("UpdateTaskScreen")
+        }) {
             Text("Update")
         }
         Spacer(Modifier.width(16.dp))
-        Button(onClick = {tasksViewModel.destroyTask(task.id)}) {
+        Button(onClick = { openDialogDestroyConfirm = !openDialogDestroyConfirm }
+        ) {
             Text("Delete")
+        }
+        if (openDialogDestroyConfirm) {
+            DialogConfirm(
+                onDismissRequest = { openDialogDestroyConfirm = !openDialogDestroyConfirm },
+                onConfirmation = { tasksViewModel.destroyTask(task.id)
+                    openDialogDestroyConfirm = !openDialogDestroyConfirm},
+                dialogTitle = "Delete task?"
+            )
+        }
+    }
+}
+
+@Composable
+fun DialogConfirm(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text = dialogTitle,
+                    modifier = Modifier.padding(16.dp))
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp), horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = { onConfirmation() }) {
+                        Text("Confirm")
+                    }
+                    Spacer(Modifier.width(16.dp))
+                    Button(onClick = { onDismissRequest() }) {
+                        Text("Cancel")
+                    }
+                }
+            }
         }
     }
 }
